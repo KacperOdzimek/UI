af6d974589293d6a29a65e3b77d390080cd2ddba
Kacper Odzimek <kacper-odzimek@wp.pl>
2026-03-25 21:27:49 +0100

    Transformations Copied

--- include/ui/ui.h
+++ include/ui/ui.h
@@ -6,18 +6,54 @@
 #define UI_H
 
 #include <stddef.h>
+#include <math.h>
 
 // ===========================
-// Transformations Typedefs
+// Layout Length
 
 const static unsigned int ui_inf_length = 0xFFFFFFFF;
 
+// structure representing 1d length
 typedef struct ui_length {
     unsigned int des;  // desired dimension
     unsigned int min;  // minimum dimension
     unsigned int max;  // maximum dimension
     float        flex; // flex ratio
 } ui_length;
+
+// ===========================
+// Render Transforms
+
+// structure representing ui elements tranformations (matrix 2x3)
+typedef struct ui_transform {
+    float m00, m01, tx;
+    float m10, m11, ty;
+} ui_transform;
+
+// Default ui element transform
+// offset: (0, 0), scale: (1, 1), rotation: (0)
+static const ui_transform ui_default_trans = {
+    1, 0, 0,
+    0, 1, 0
+};
+
+// runtime transformations
+
+// build transform:
+// identity -> rotate(deg_cw) -> scale(sx, sy) -> offset(dx, dy)
+// UI_TRANS <- compile time alternative
+static inline ui_transform ui_trans(float dx, float dy, float sx, float sy, float deg_cw);
+
+// translate/offset transform by dx, dy normalized units
+static inline ui_transform ui_off(ui_transform m, float dx, float dy);
+
+// scale transform by sx in x and sy in y
+static inline ui_transform ui_sca(ui_transform m, float sx, float sy);
+
+// rotate transform by degrees clockwise
+static inline ui_transform ui_rot(ui_transform m, float deg_cw);
+
+// UI_TRANS <- compile time ui_trans transform builder macro (later in the file)
 
 // ===========================
 // Node Typedef
@@ -70,7 +106,7 @@ typedef struct ui_sizebox_data {
 
 typedef struct ui_row_data {
     float     align;    // 0 - align left, 0.5 - align center, 1.0 - align right, other values also work
-    ui_length spacing;
+    ui_length spacing;  // spacing between childrens
 } ui_row_data;
 
 // ===========================
@@ -88,6 +124,132 @@ typedef struct ui_tree_info {
 
 size_t ui_subtree(const ui_node* node);
 void   ui_measure(ui_tree_info* ti);
+void   ui_render (ui_tree_info* ti);
+
+// ===========================
+// Transformations Implemenations
+
+static inline ui_transform ui_trans(float offx, float offy, float scalex, float scaley, float deg_cw) {
+    float rad = deg_cw * (3.14159265358979323846f / 180.0f);
+
+    #ifdef UI_IMPL_INVERT_ROTATION
+        float cosr = cosf(rad);
+        float sinr = -sinf(rad); // invert rotation
+    #else
+        float cosr = cosf(rad);
+        float sinr = sinf(rad);  // CW rotation by default
+    #endif
+
+    return (ui_transform){
+        .m00 = cosr * scalex,
+        .m01 = sinr * scaley,
+        .tx  = offx,
+        .m10 = -sinr * scalex,
+        .m11 = cosr * scaley,
+        .ty  = offy
+    };
+}
+
+static inline ui_transform ui_off(ui_transform m, float dx, float dy) {
+    m.tx += dx * m.m00 + dy * m.m01;
+    m.ty += dx * m.m10 + dy * m.m11;
+    return m;
+}
+
+static inline ui_transform ui_sca(ui_transform m, float sx, float sy) {
+    m.m00 *= sx;  m.m01 *= sy;
+    m.m10 *= sx;  m.m11 *= sy;
+
+    m.m00 *= sx; m.m10 *= sx;
+    m.m01 *= sy; m.m11 *= sy;
+
+    return m;
+}
+
+static inline ui_transform ui_rot(ui_transform m, float deg_cw) {
+    float rad = deg_cw * (3.14159265358979323846f / 180.0f);
+
+#ifdef UI_IMPL_INVERT_ROTATION
+    float cosr = cosf(rad);
+    float sinr = sinf(rad);
+#else
+    float cosr = cosf(rad);
+    float sinr = -sinf(rad);
+#endif
+
+    float m00 = m.m00 * cosr + m.m01 * sinr;
+    float m01 = -m.m00 * sinr + m.m01 * cosr;
+    float m10 = m.m10 * cosr + m.m11 * sinr;
+    float m11 = -m.m10 * sinr + m.m11 * cosr;
+
+    m.m00 = m00; m.m01 = m01;
+    m.m10 = m10; m.m11 = m11;
+    return m;
+}
+
+static inline ui_transform ui_mul(ui_transform p, ui_transform c) {
+    ui_transform r;
+
+    r.m00 = p.m00 * c.m00 + p.m01 * c.m10;
+    r.m01 = p.m00 * c.m01 + p.m01 * c.m11;
+    r.tx  = p.m00 * c.tx  + p.m01 * c.ty + p.tx;
+
+    r.m10 = p.m10 * c.m00 + p.m11 * c.m10;
+    r.m11 = p.m10 * c.m01 + p.m11 * c.m11;
+    r.ty  = p.m10 * c.tx  + p.m11 * c.ty + p.ty;
+
+    return r;
+}
+
+// compile time fmod function
+#define UI_FMOD_APPROX(x, m) \
+    ( ((x) >= 0.0f) ? ((x) - (m) * (int)((x)/(m))) : ((x) - (m) * ((int)((x)/(m)) - 1)) )
+
+#ifdef UI_IMPL_INVERT_ROTATION
+// compile time deg to rad conversion
+#define DEG_TO_RAD(deg_cw) \
+    ((((UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) > 3.14159265f) ? \
+        ((UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) - 6.28318531f) : \
+        (((UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) < -3.14159265f) ? \
+            ((UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) + 6.28318531f) : \
+            (UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f))))
+#else
+// compile time deg to rad conversion
+#define UI_DEG_TO_RAD(deg_cw) \
+    (((( -UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) > 3.14159265f) ? \
+        ((-UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) - 6.28318531f) : \
+        (((-UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) < -3.14159265f) ? \
+            ((-UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f) + 6.28318531f) : \
+            (-UI_FMOD_APPROX((deg_cw), 360.0f) * 0.017453292519943295f))))
+#endif
+
+// compile time sin approx (x - rad)
+#define UI_SIN_APPROX(x) ((x) \
+  - ((x)*(x)*(x))/6.0 \
+  + ((x)*(x)*(x)*(x)*(x))/120.0 \
+  - ((x)*(x)*(x)*(x)*(x)*(x)*(x))/5040.0 \
+  + ((x)*(x)*(x)*(x)*(x)*(x)*(x)*(x)*(x))/362880.0 \
+  - ((x)*(x)*(x)*(x)*(x)*(x)*(x)*(x)*(x)*(x)*(x))/39916800.0)
+
+// compile time cos approx (x - rad)
+#define UI_COS_APPROX(x) (1.0 \
+  - ((x)*(x))/2.0 \
+  + ((x)*(x)*(x)*(x))/24.0 \
+  - ((x)*(x)*(x)*(x)*(x)*(x))/720.0 \
+  + ((x)*(x)*(x)*(x)*(x)*(x)*(x)*(x))/40320.0 \
+  - ((x)*(x)*(x)*(x)*(x)*(x)*(x)*(x)*(x)*(x))/3628800.0)
+
+// compile time tranformation matrix builder
+// transformation order: rotate, scale, translate
+#define UI_TRANS(offx, offy, scax, scay, deg_cw) \
+    (ui_transform){ \
+        .m00 = UI_COS_APPROX(UI_DEG_TO_RAD(deg_cw)) * (scax), \
+        .m01 = -UI_SIN_APPROX(UI_DEG_TO_RAD(deg_cw)) * (scay), \
+        .tx  = (offx), \
+        .m10 = UI_SIN_APPROX(UI_DEG_TO_RAD(deg_cw)) * (scax), \
+        .m11 = UI_COS_APPROX(UI_DEG_TO_RAD(deg_cw)) * (scay), \
+        .ty  = (offy) \
+    }
 
 #endif
 
@@ -240,6 +402,22 @@ static size_t measure_dispatch(ui_tree_info* ti, const ui_node* node, size_t idx
 
 void ui_measure(ui_tree_info* ti) {
     measure_dispatch(ti, ti->root, 0);
+}
+
+// ===========================
+// Rendering
+
+static size_t render_dispatch(ui_tree_info* ti, const ui_node* node, size_t idx);
+
+
+static size_t render_dispatch(ui_tree_info* ti, const ui_node* node, size_t idx) {
+    switch (node->type) {
+    
+    }
+}
+
+void ui_render(ui_tree_info* ti) {
+
 }
 
 #endif
